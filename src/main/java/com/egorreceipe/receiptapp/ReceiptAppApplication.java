b43bb30e5bcda4f75e67226ca45a9354ac4f5695
egorcouldlie <couldliegor@gmail.com>
2023-02-03 02:51:03 +0300

    Master

---
+++ src/main/java/com/egorreceipe/receiptapp/ReceiptAppApplication.java
@@ -0,0 +1,13 @@
+package com.egorreceipe.receiptapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceiptAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReceiptAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/egorreceipe/receiptapp/ReceiptAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.egorreceipe.receiptapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceiptAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
